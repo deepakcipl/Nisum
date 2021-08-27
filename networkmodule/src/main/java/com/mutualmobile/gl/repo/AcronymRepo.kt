cb9e94624bbb827fc8335175f0b2a6ae13917d03
Deepak Sharma <deepak.sharma@necam.com>
2021-08-27 18:16:29 +0530

    Git Commit

--- networkmodule/src/main/java/com/mutualmobile/gl/repo/AcronymRepo.kt
+++ networkmodule/src/main/java/com/mutualmobile/gl/repo/AcronymRepo.kt
@@ -3,7 +3,7 @@ package com.mutualmobile.gl.repo
 import com.mutualmobile.gl.data.model.news.acronym.Json4KotlinAcronymBase
 import com.mutualmobile.gl.NetworkResult
 import com.mutualmobile.gl.data.services.CoroutineApiService
-
+// Git
 class AcronymRepo constructor(private val coroutineApiService: CoroutineApiService) {
 
     suspend fun getDictionary(name: String): NetworkResult<List<Json4KotlinAcronymBase>> {
