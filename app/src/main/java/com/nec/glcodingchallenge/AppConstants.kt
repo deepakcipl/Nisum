76bcf5a2c4572bdd3b4805f1c59907b4841bc9c5
Deepak Sharma <deepak.sharma@necam.com>
2021-08-28 13:02:23 +0530

    Update Base url.

--- app/src/main/java/com/nec/glcodingchallenge/AppConstants.kt
+++ app/src/main/java/com/nec/glcodingchallenge/AppConstants.kt
@@ -1,7 +1,6 @@
 package com.nec.glcodingchallenge
 
 object AppConstants {
- // const val BASE_URL = "https://newsapi.org/"
   const val BASE_URL = "http://www.nactem.ac.uk/"
 
   const val COROUTINE_RETROFIT = "COROUTINE_RETROFIT"
